10a22c05c2ff6ef0dd2524f28bef5b87af2b33f5
Birame Sene <bsene.pro@gmail.com>
2026-08-07 04:11:21 +0100

    test(deno): adding snapshot (approvals)

--- deno/main_test.ts
+++ deno/main_test.ts
@@ -3,16 +3,17 @@ import { statement } from "./main.ts";
 import { invoiceSchema } from "./validation/invoice.schema.ts";
 import { playReferenceSchema } from "./validation/play.schema.ts";
 
-Deno.test({
-  name: "generate invoice",
-  fn: async () => {
-    const invoice = invoiceSchema.parse(
-      JSON.parse(await Deno.readTextFile("./assets/invoice.json")),
-    );
-    const plays = playReferenceSchema.parse(
-      JSON.parse(await Deno.readTextFile("./assets/plays.json")),
-    );
+Deno.test("generate invoice", async () => {
+  const invoice = invoiceSchema.parse(
+    JSON.parse(await Deno.readTextFile("./assets/invoice.json")),
+  );
+  const plays = playReferenceSchema.parse(
+    JSON.parse(await Deno.readTextFile("./assets/plays.json")),
+  );
 
-    expect(statement(invoice, plays), "the content of the file");
-  },
+  expect.setState({
+    currentTestName: "generate valid invoice",
+    testPath: import.meta.url,
+  });
+  expect(statement(invoice, plays)).toMatchSnapshot();
 });
